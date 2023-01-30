1bf4105902b242a2c756b6a5f4e8b7f6341a7b00
rduarte01 <rduarte01@hotmail.com>
2023-01-30 09:28:18 -0300

    Crear nuestros archivos de proyecto

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {NAv, NavbarContainer, NavLogo} from './NavbarElements'
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NacbarContainer>
+            <Navlogo>
+                dolla
+            </Navlogo>
+        </NacbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
